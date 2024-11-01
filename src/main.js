3ba6d662ab3d855d61bda90566302e60b3bf392e
Waqas Tahir <wqs.wtu@gmail.com>
2024-11-01 23:04:46 +0500

    Organized router files.

--- src/main.js
+++ src/main.js
@@ -1,62 +1,8 @@
 
-import { createRouter, createWebHistory } from 'vue-router';
 import { createApp } from 'vue';
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
-import TeamMembers from './components/teams/TeamMembers.vue';
-import NotFound from './components/nav/NotFound.vue';
-import TeamsFooter from './components/teams/TeamsFooter.vue';
-import UsersFooter from './components/users/UsersFooter.vue';
+import router from './router.js'
 
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [
-        // {path:'/',redirect:'/teams'},
-        {
-            name: 'team', path: '/teams', 
-            meta:{needsAuth:true},
-            components: { default: TeamsList, footer: TeamsFooter }, alias: '/', children: [
-
-                { name: 'team-members', path: ':apniTeam', component: TeamMembers, props: true },
-            ]
-        },
-        {
-            path: '/users', components: { default: UsersList, footer: UsersFooter },
-            beforeEnter(to, from, next) {
-                console.log('User before enter')
-                console.log(to, from)
-                next()
-            }
-        },
-
-
-        { path: '/:notFound(.*)', component: NotFound },
-
-    ],
-    linkActiveClass: "active",
-    scrollBehavior(_to, _from, savedPosition) {
-        // console.log(to,from,savedPosition)
-        if (savedPosition) {
-            return savedPosition;
-        }
-        return { left: 0, top: 0 }
-    }
-})
-router.beforeEach(function (to, from, next) {
-    console.log('before Each Guard at main.js')
-    console.log(to, from)
-    if(to.meta.needsAuth){
-        next()
-    }else{
-        next()
-    }
-    
-})
-router.afterEach(function (to, from) {
-    console.log('after each guard at main.js')
-    console.log(to, from)
-})
 
 
 const app = createApp(App)
